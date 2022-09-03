f358dcb123684cbf8bdd4b5d8126efbd1258bdd8
BROWN <eqbrown@tea-2-eqbrown.lan>
2022-09-03 21:34:56 +0800

    init commit student game file

---
+++ game.py
@@ -0,0 +1,23 @@
+#####################################################
+# interface for the Riddler, a riddle guessing game
+# ----------
+# it's up to you to finish it!
+# feel free to customize it as you please.
+######################################################
+
+# Imports a list of Riddle objects from riddle.py
+from riddle import riddles
+
+def game_loop():
+    '''This function runs the game.'''
+
+    print("-"*35)
+    print("---- Welcome to the Riddler ----")
+    print("-"*35,"\n")
+
+   # 💻 YOUR CODE GOES BELOW HERE 💻 #
+
+
+
+if __name__=='__main__':
+    game_loop()
