33f5d7f6636739bb74800bfa7703e1856624fd43
emma <eqbrown@isf.edu.hk>
2022-09-04 10:52:40 +0800

    simply properties

--- riddle.py
+++ riddle.py
@@ -5,18 +5,16 @@ class Riddle:
     def __init__(self,prompt,answer):
         self.prompt = prompt
         self.answer = answer
-  
-        # this sets the sensitivty to accept guesses close to the answer
-        self.MIN_FUZZ_RATIO = 75
 
 
     def check_guess(self,guess):
         """Checks whether a guess is correct.
         Uses the fuzzywuzzy library to accept guesses which are close to the answer.
         """
+        min_fuzz_ratio = 80
         similarity = fuzz.ratio(guess.lower(), self.answer.lower())
         
-        if similarity >= self.MIN_FUZZ_RATIO:
+        if similarity >= min_fuzz_ratio:
             return True
         else:
             return False
