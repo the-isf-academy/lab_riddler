1e4979f61196651428aa4587e57b9e1887919f45
brittegenzlinger <britte.genzlinger@gmail.com>
2024-08-22 08:46:48 +0800

    update leveshtein package in toml

--- riddle.py
+++ riddle.py
@@ -6,10 +6,9 @@ class Riddle:
         self.answer = answer
 
     def check_guess(self,guess):
-        """Checks whether a guess is correct.
-        Uses the fuzzywuzzy library to accept guesses which 
-        are close to the answer.
-        """
+        #Checks whether a guess is correct.
+        #Uses the fuzzywuzzy library to accept guesses which 
+        #are close to the answer.
         
         min_fuzz_ratio = 80
         similarity = fuzz.ratio(guess.lower(), self.answer.lower())
